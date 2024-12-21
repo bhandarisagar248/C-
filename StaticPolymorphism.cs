a5ee6273874e1cd721e7ec5ad4f49fac71ed93f1
Sagar Bhandari <bhandarisagar248@gmail.com>
2024-12-21 11:54:08 +0545

    Static Polymorphism

---
+++ StaticPolymorphism.cs
@@ -0,0 +1,35 @@
+// using System;
+// namespace StaticPolymorphism{
+
+// public class Student{
+
+// public void print(String name){
+//     Console.WriteLine("Name:"+name);
+// }
+
+// public void print(int id){
+//     Console.WriteLine("ID:"+id);
+// }
+
+// public void print(double phone){
+//     Console.WriteLine("Phone:"+phone);
+// }
+
+// public static void Main(String[] args){
+// Student std=new Student();
+
+// std.print("Ramesh Shrestha");
+// std.print(21);
+// std.print(9841320246.0);
+
+
+// }
+
+
+
+// }
+
+
+
+
+// }
